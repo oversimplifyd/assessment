c193133cdbf41883c7407a5b62df5e5c55fd2ae3
Busayo <arotimi.busayo@gmail.com>
2017-09-14 19:38:35 -0400

    Updated Requests

--- app/Http/Requests/APIServerCreateRequest.php
+++ app/Http/Requests/APIServerCreateRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ServerCreateRequest extends FormRequest
+class APIServerCreateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
