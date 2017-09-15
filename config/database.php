9c1d41b389987499d15193a7bc45927f666b403c
Busayo <arotimi.busayo@gmail.com>
2017-09-14 23:34:51 -0400

    Added Deployment for Heroku

--- config/database.php
+++ config/database.php
@@ -39,6 +39,17 @@ return [
             'prefix' => '',
         ],
 
+        'heroku' => [
+            'driver' => 'pgsql',
+            'host' => parse_url(getenv("DATABASE_URL"))["host"],
+            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+            'username' => parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
